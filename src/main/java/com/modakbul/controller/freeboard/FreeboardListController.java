88ba4b038fff174a7c818984d49df9ae901f5fad
leesomang <dlthakd2222@naver.com>
2024-10-04 13:37:01 +0900

    [rename] 자유게시판list 경로수정 및 멤버값추가

--- src/main/java/com/modakbul/controller/freeboard/FreeboardListController.java
+++ src/main/java/com/modakbul/controller/freeboard/FreeboardListController.java
@@ -1,14 +1,21 @@
 package com.modakbul.controller.freeboard;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.modakbul.security.CustomUserDetails;
+
 @Controller
-public class FreeBoardListController {
+public class FreeboardListController {
 	
 	@GetMapping("/freeboard/freeBoardList")
-	public String board(Model model) {
+	public String board(@AuthenticationPrincipal CustomUserDetails member, Model model) {
+		if (member != null) {
+	        model.addAttribute("member", member);
+	    }
+		
 		return "freeboard/freeBoardList";
 	}
 	
