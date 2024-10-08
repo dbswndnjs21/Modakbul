60ada59a51b8d15eb05a2faa10303ea6e198e377
aflyingmole <ljhkyj25@gmail.com>
2024-10-08 16:51:09 +0900

    [modify] 사용자 정보 업데이트 로직 추가

--- src/main/java/com/modakbul/controller/MyPageController.java
+++ src/main/java/com/modakbul/controller/MyPageController.java
@@ -1,6 +1,9 @@
 package com.modakbul.controller;
 
+import com.modakbul.entity.member.Member;
 import com.modakbul.security.CustomUserDetails;
+import com.modakbul.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/mypage")
 public class MyPageController {
+    @Autowired
+    private MemberService memberService;
 
     @GetMapping("")
     public String myPage(@AuthenticationPrincipal CustomUserDetails member, Model model) {
@@ -47,15 +52,21 @@ public class MyPageController {
     }
 
     @PostMapping("/updateInfo")
-    public String updatePersonalInfo(@AuthenticationPrincipal CustomUserDetails member,
+    public String updatePersonalInfo(@AuthenticationPrincipal CustomUserDetails memberDetails,
                                      @RequestParam("username") String username,
-                                     @RequestParam("email") String email,
                                      @RequestParam("password") String password,
                                      Model model) {
-        if (member != null) {
-            // 사용자 정보 업데이트 로직 추가
-            System.out.println("회원 정보 업데이트: " + username + ", " + email);
-            // 업데이트가 완료되면 마이페이지로 리다이렉트
+        if (memberDetails != null) {
+            // DB에서 해당 유저 정보를 가져옴
+            Member member = memberService.findByUserId(memberDetails.getUsername());
+
+            // 사용자 정보 업데이트
+            memberService.updateMemberInfo(member, username, password);
+
+            // 업데이트 후 로그
+            System.out.println("회원 정보 업데이트: " + username);
+
+            // 업데이트 완료 후 마이페이지로 리다이렉트
             return "redirect:/mypage/myPage";
         }
         return "redirect:/login";
