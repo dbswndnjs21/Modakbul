2761776e01fc752155c36e9fc3f638ee8507fe2d
aflyingmole <ljhkyj25@gmail.com>
2024-10-17 14:44:10 +0900

    [modify] 멤버서비스 수정

--- src/main/java/com/modakbul/service/member/MemberService.java
+++ src/main/java/com/modakbul/service/member/MemberService.java
@@ -65,4 +65,8 @@ public class MemberService {
     public Member findById(Long id) {
         return memberRepository.findById(id).get();
     }
+    
+    public Member findMembership(String userId) {
+        return memberRepository.findByUserId(userId);
+    }
 }
