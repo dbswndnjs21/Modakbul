3e92a69f61db5ffe92ca4a24b69b193844e86a05
aflyingmole <ljhkyj25@gmail.com>
2024-10-21 16:30:47 +0900

    [feat]호스트 전환 추가

--- src/main/java/com/modakbul/service/member/HostService.java
+++ src/main/java/com/modakbul/service/member/HostService.java
@@ -3,6 +3,7 @@ package com.modakbul.service.member;
 import com.modakbul.entity.member.Host;
 import com.modakbul.entity.member.Member;
 import com.modakbul.repository.member.HostRepository;
+import com.modakbul.repository.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,14 @@ import org.springframework.stereotype.Service;
 public class HostService {
     @Autowired
     private HostRepository hostRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public void save(Host host){
+        memberRepository.save(host.getMember());
+        hostRepository.save(host);
+
+    }
 
     public Host findById(Long id) {
         return hostRepository.findById(id).orElse(null);
--- src/main/java/com/modakbul/service/member/MemberService.java
+++ src/main/java/com/modakbul/service/member/MemberService.java
@@ -2,12 +2,16 @@ package com.modakbul.service.member;
 
 import com.modakbul.entity.member.Member;
 import com.modakbul.repository.member.MemberRepository;
+import com.modakbul.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -24,6 +28,24 @@ public class MemberService {
         String encodePassword = passwordEncoder.encode(password); // 암호화
         member.setPassword(encodePassword);
         memberRepository.save(member);
+    }
+    public void saveHost(Member member){
+        Optional<Member> byId = memberRepository.findById(member.getId());
+        System.out.println(member);
+        System.out.println("member.getid() + " + member.getId());
+
+        Member member1 = byId.get();
+
+        member1.setId(member.getId());
+        member1.setCreatedAt(member.getCreatedAt());
+        member1.setMail(member.getMail());
+        member1.setPassword(member.getPassword());
+        member1.setProvider(member.getProvider());
+        member1.setRole("MEMBERHOST");
+        member1.setUpdatedAt(LocalDateTime.now());
+        member1.setUserId(member.getUserId());
+        member1.setMembership(member.getMembership());
+        memberRepository.save(member1);
     }
 
     // 사용자 정보 업데이트 메서드 추가
