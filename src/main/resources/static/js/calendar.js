c1037166fdb6a57ca815a529fa94068778d76016
Carpeii <rlawldnd1206@gmail.com>
2024-10-31 14:20:42 +0900

    [refactor] 달력 리팩토링 및 편의성 개선

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -8,7 +8,7 @@ let isCheckInSelected = false;
 let isCheckOutSelected = false;
 
 document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
+    const calendarEl = document.getElementById('calendar');
 
     // URL에서 체크인, 체크아웃 날짜 가져오기
     const urlParams = new URLSearchParams(window.location.search);
@@ -20,113 +20,102 @@ document.addEventListener('DOMContentLoaded', function () {
     maxDate.setDate(today.getDate() + 92); // 오늘 날짜로부터 92일 후
 
     // 날짜를 input 요소에 설정
-    document.getElementById('checkInDate').value = checkInDate;
-    document.getElementById('checkOutDate').value = checkOutDate;
-
-    // 화면에 표시될 날짜 설정
-    document.getElementById('checkInDisplay').innerText = checkInDate;
-    document.getElementById('checkOutDisplay').innerText = checkOutDate;
+    updateDateInputs();
 
     calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         selectable: true,
-        select: function (info) {
-            var startDateStr = info.startStr;
-            var startDate = new Date(info.startStr);
-            if (startDate < today || startDate > maxDate) {
-                return;
-            }
-            // 체크아웃이 선택된 상태일 때 새로운 체크인 날짜를 선택하도록 설정
-            if (isCheckOutSelected) {
-                checkInDate = startDateStr;
-                checkOutDate = new Date(new Date(checkInDate).setDate(new Date(checkInDate).getDate() + 1)).toISOString().slice(0, 10);
-                isCheckOutSelected = false;
-            } else {
-                // 체크인 날짜가 아직 선택되지 않은 경우 체크인 날짜로 설정
-                if (!isCheckInSelected) {
-                    checkInDate = startDateStr;
-                    checkOutDate = new Date(new Date(checkInDate).setDate(new Date(checkInDate).getDate() + 1)).toISOString().slice(0, 10); // 체크아웃을 1박 2일 후로 설정
-                    isCheckInSelected = true; // 체크인 날짜가 설정되었음을 표시
-                } else {
-                    // 체크아웃 날짜는 체크인 이후 날짜만 선택 가능
-                    if (new Date(startDate) > new Date(checkInDate)) {
-                        checkOutDate = startDateStr;
-                        isCheckOutSelected = true; // 체크아웃이 선택되었음을 표시
-                    }
-                }
-            }
-
-            // 날짜 업데이트
-            document.getElementById('checkInDate').value = checkInDate;
-            document.getElementById('checkOutDate').value = checkOutDate;
-
-            document.getElementById('checkInDisplay').innerText = checkInDate;
-            document.getElementById('checkOutDisplay').innerText = checkOutDate;
-
-            // 선택된 날짜 스타일링을 초기화하고 새롭게 반영
-            calendar.getEvents().forEach(event => event.remove());
-
-            if (checkInDate) {
-                // 체크인 날짜 표시
-                calendar.addEvent({
-                    start: checkInDate,
-                    end: new Date(new Date(checkOutDate).setDate(new Date(checkOutDate).getDate() + 1)).toISOString().slice(0, 10),
-                    display: 'background',
-                    classNames: ['selected']
-                });
-            }
-
-            if (checkInDate && checkOutDate) {
-                // 체크인과 체크아웃 사이의 날짜 범위 표시
-                calendar.addEvent({
-                    start: new Date(new Date(checkInDate).setDate(new Date(checkInDate).getDate() + 1)).toISOString().slice(0, 10),
-                    end: new Date(checkOutDate).toISOString().slice(0, 10),
-                    display: 'background',
-                    classNames: ['selected']
-                });
-            }
-        },
+        select: handleDateSelect,
         unselectAuto: false
     });
 
     // 초기 상태에서 체크인 및 체크아웃 날짜 반영
-    calendar.addEvent({
-        start: checkInDate,
-        end: new Date(new Date(checkOutDate).setDate(new Date(checkOutDate).getDate() + 1)).toISOString().slice(0, 10),
-        display: 'background',
-        classNames: ['selected']
-    });
-
-    if (checkInDate && checkOutDate) {
-        calendar.addEvent({
-            start: new Date(new Date(checkInDate).setDate(new Date(checkInDate).getDate() + 1)).toISOString().slice(0, 10),
-            end: new Date(checkOutDate).toISOString().slice(0, 10),
-            display: 'background',
-            classNames: ['selected']
-        });
-    }
+    addCalendarEvents();
 
     calendar.render();
 });
 
 toggleButton.addEventListener('click', function () {
-    if (calendarDiv.style.display === 'none') {
-        calendarDiv.style.display = 'block';
-        calendar.render(); // FullCalendar를 다시 렌더링
-        toggleButton.textContent = '숨기기'; // 버튼 텍스트 변경
-    } else {
-        calendarDiv.style.display = 'none';
-        toggleButton.textContent = '보이기'; // 버튼 텍스트 변경
-    }
+    const isHidden = calendarDiv.style.display === 'none';
+    calendarDiv.style.display = isHidden ? 'block' : 'none';
+    toggleButton.textContent = isHidden ? '숨기기' : '보이기'; // 버튼 텍스트 변경
+    if (isHidden) calendar.render(); // FullCalendar를 다시 렌더링
 });
+
+function updateDateInputs() {
+    document.getElementById('checkInDate').value = checkInDate;
+    document.getElementById('checkOutDate').value = checkOutDate;
+    document.getElementById('checkInDisplay').innerText = checkInDate;
+    document.getElementById('checkOutDisplay').innerText = checkOutDate;
+}
+
+function handleDateSelect(info) {
+    const selectedDate = new Date(info.startStr);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // 오늘 날짜의 시간 부분을 0으로 설정
+
+    // 체크인 날짜로 오늘 날짜 및 이후 날짜만 허용
+    if (selectedDate < today || selectedDate > new Date().setDate(today.getDate() + 92)) {
+        return; // 오늘 이전 날짜 또는 최대 선택 가능 날짜를 초과한 경우
+    }
+
+    // 체크인 날짜가 이미 선택된 상태에서 선택된 날짜가 체크인 날짜 이전일 경우
+    if (isCheckInSelected && selectedDate < new Date(checkInDate)) {
+        // 기존 체크인 날짜를 체크아웃으로 설정
+        checkOutDate = checkInDate; // 기존 체크인 날짜를 체크아웃 날짜로 설정
+        checkInDate = info.startStr; // 새로 선택된 날짜를 체크인 날짜로 설정
+        isCheckOutSelected = true; // 체크아웃이 선택되었음을 표시
+        isCheckInSelected = true; // 체크인 날짜가 설정되었음을 유지
+    } else {
+        // 체크아웃이 선택된 상태인 경우
+        if (isCheckOutSelected) {
+            checkInDate = info.startStr; // 체크인 날짜를 선택된 날짜로 설정
+            checkOutDate = new Date(selectedDate.setDate(selectedDate.getDate() + 1)).toISOString().slice(0, 10); // 체크아웃 날짜를 체크인 날짜 + 1일로 설정
+            isCheckOutSelected = false;
+        } else {
+            // 체크인 날짜가 선택되지 않은 경우
+            if (!isCheckInSelected) {
+                checkInDate = info.startStr; // 체크인 날짜를 선택된 날짜로 설정
+                checkOutDate = new Date(selectedDate.setDate(selectedDate.getDate() + 1)).toISOString().slice(0, 10); // 체크아웃 날짜를 체크인 날짜 + 1일로 설정
+                isCheckInSelected = true; // 체크인 날짜가 설정되었음을 표시
+            } else if (selectedDate > new Date(checkInDate)) {
+                checkOutDate = info.startStr; // 체크아웃 날짜를 선택된 날짜로 설정
+                isCheckOutSelected = true; // 체크아웃이 선택되었음을 표시
+            }
+        }
+    }
+
+    updateDateInputs();
+    addCalendarEvents();
+}
+
+function addCalendarEvents() {
+    calendar.getEvents().forEach(event => event.remove());
+
+    if (checkInDate) {
+        calendar.addEvent({
+            start: checkInDate,
+            end: new Date(new Date(checkOutDate).setDate(new Date(checkOutDate).getDate() + 1)).toISOString().slice(0, 10),
+            display: 'background',
+            classNames: ['selected']
+        });
+    }
+
+    if (checkInDate && checkOutDate) {
+        calendar.addEvent({
+            start: new Date(checkInDate),
+            end: new Date(checkOutDate),
+            display: 'background',
+            classNames: ['selected']
+        });
+    }
+}
 
 function validateForm() {
     return true; // 폼을 제출할 수 있도록 true 반환
 }
 
 function setSelectedCampsite(campsiteId) {
-    // Set the campsite ID in the hidden input field
     document.getElementById('campsiteId').value = campsiteId;
-    // Automatically submit the form
     document.querySelector('form').submit();
 }
